bdc3913e706c1616a1cc2b2c18911b970068ca7c
User <mihailovic023@gmail.com>
2025-02-10 00:58:28 +0100

    Home page redirects to register if user does not exist.

--- ExpenseTracker/Controllers/HomeController.cs
+++ ExpenseTracker/Controllers/HomeController.cs
@@ -17,35 +17,44 @@ namespace ExpenseTracker.Controllers
         private readonly UserManager<User> _userManager;
         private readonly IIncomeService _incomeService;
         private readonly IExpenseService _expenseService;
-        public HomeController(ILogger<HomeController> logger, ICommonMethods commonMethods, UserManager<User> userManager,IIncomeService incomeService, IExpenseService expenseService)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public HomeController(ILogger<HomeController> logger, ICommonMethods commonMethods, UserManager<User> userManager,IIncomeService incomeService, IExpenseService expenseService, IHttpContextAccessor httpContextAccessor)
         {
             _logger = logger;
             _commonMethods = commonMethods;
             _userManager = userManager;
             _incomeService = incomeService;
             _expenseService = expenseService;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<IActionResult> Index()
         {
-            var incomeChartData = await _incomeService.GetIncomeChartDataAsync();
-            var expenseChartData = await _expenseService.GetExpenseChartDataAsync();
+            var user = await _userManager.GetUserAsync(_httpContextAccessor.HttpContext?.User);
+            if (user != null)
+            {
+                var incomeChartData = await _incomeService.GetIncomeChartDataAsync();
+                var expenseChartData = await _expenseService.GetExpenseChartDataAsync();
 
-            ViewBag.Labels = incomeChartData.Select(c => c.SourceName).ToArray();
-            ViewBag.Data = incomeChartData.Select(c => c.TotalIncome).ToArray();
-            ViewBag.ExpenseLabels = expenseChartData.Select(e => e.SourceName).ToArray();
-            ViewBag.ExpenseData = expenseChartData.Select(e => e.TotalIncome).ToArray();
+                ViewBag.Labels = incomeChartData.Select(c => c.SourceName).ToArray();
+                ViewBag.Data = incomeChartData.Select(c => c.TotalIncome).ToArray();
+                ViewBag.ExpenseLabels = expenseChartData.Select(e => e.SourceName).ToArray();
+                ViewBag.ExpenseData = expenseChartData.Select(e => e.TotalIncome).ToArray();
 
-            decimal totalIncome = await _incomeService.GetAllIncomeSum(); 
-            decimal totalExpense = await _expenseService.GetAllExpenseSum(); 
-            decimal balance = totalIncome - totalExpense;
-            decimal allowedMinus = 500; 
+                decimal totalIncome = await _incomeService.GetAllIncomeSum();
+                decimal totalExpense = await _expenseService.GetAllExpenseSum();
+                decimal balance = totalIncome - totalExpense;
+                decimal allowedMinus = 500;
 
-            ViewBag.TotalIncome = totalIncome;
-            ViewBag.TotalExpense = totalExpense;
-            ViewBag.Balance = balance;
-            ViewBag.AllowedMinus = allowedMinus;
-
+                ViewBag.TotalIncome = totalIncome;
+                ViewBag.TotalExpense = totalExpense;
+                ViewBag.Balance = balance;
+                ViewBag.AllowedMinus = allowedMinus;
+            }
+            else
+            {
+                return Redirect("/Identity/Account/Register");
+            }
             return View();
         }
         [Authorize(Roles = "user")]
